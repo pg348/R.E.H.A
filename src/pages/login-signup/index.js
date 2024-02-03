dc98b42f5010cfdad148e4ee289d4d59298e3233
amansagar0403 <as624@snu.edu.in>
2024-02-03 15:08:41 +0530

    login header added

--- src/pages/login-signup/index.js
+++ src/pages/login-signup/index.js
@@ -6,7 +6,7 @@ import { meta } from "../../content_option";
 import { Container, Row, Col, Alert } from "react-bootstrap";
 import { contactConfig } from "../../content_option";
 
-export const ContactUs = () => {
+export const LoginSignUp = () => {
   const [formData, setFormdata] = useState({
     email: "",
     name: "",
