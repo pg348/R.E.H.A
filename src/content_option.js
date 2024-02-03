549a03a4699ae2e81815f2767a6a643b745d780e
AviralChawla20 <aviralchawla02@gmail.com>
2024-02-03 13:21:28 +0530

    initial commit

---
+++ src/content_option.js
@@ -0,0 +1,108 @@
+const logotext = "RHEA";
+const meta = {
+    title: "Realistic Human Emulation Assistant",
+    description: "I’m Rhea ",
+};
+
+const introdata = {
+    title: "I’m Rhea",
+    animated: {
+        first: "A virtual AI.",
+        second: "Can help with your feelings.",
+        third: "Can tell you what you feel.",
+    },
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum",
+    your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
+};
+
+const dataabout = {
+    title: "abit about my self",
+    aboutme: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent quis dolor id ligula semper elementum feugiat pretium nulla. Nunc non commodo dolor. Nunc mollis dignissim facilisis. Morbi ut magna ultricies.",
+};
+const worktimeline = [{
+        jobtitle: "Check",
+        where: "WOrkholder",
+        date: "Video",
+    },
+    {
+        jobtitle: "Check",
+        where: "WOrkholder",
+        date: "Video",
+    },
+    {
+        jobtitle: "Check",
+        where: "WOrkholder",
+        date: "Video",
+    },
+];
+
+const skills = [{
+        name: "Feelings",
+        value: 90,
+    },
+    {
+        name: "Emotion",
+        value: 85,
+    },
+    {
+        name: "Anger",
+        value: 10,
+    },
+    {
+        name: "Sad",
+        value: 40,
+    },
+    {
+        name: "Happiness",
+        value: 100,
+    },
+];
+
+const services = [{
+        title: "Emotion",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+    },
+    {
+        title: "Happiness",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+    },
+    {
+        title: "How to Check?",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+    },
+];
+
+const dataportfolio = [{
+    video: "https://www.youtube.com/watch?v=NMzb36lrFuo",
+    link: "#",
+    }
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "name@domain.com",
+    YOUR_FONE: "123456789",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vehicula eu nunc et sollicitudin. Cras pulvinar, nisi at imperdiet pharetra. ",
+    YOUR_SERVICE_ID: "service_id",
+    YOUR_TEMPLATE_ID: "template_id",
+    YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+    github: "https://github.com",
+    facebook: "https://facebook.com",
+    linkedin: "https://linkedin.com",
+    twitter: "https://twitter.com",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+    
+};
