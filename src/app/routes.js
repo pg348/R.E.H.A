760228bddde0b0ba1120ade09c8fc5838f4c3fe9
amansagar0403 <as624@snu.edu.in>
2024-02-03 15:48:15 +0530

    login done

--- src/app/routes.js
+++ src/app/routes.js
@@ -4,7 +4,7 @@ import withRouter from "../hooks/withRouter"
 import { Home } from "../pages/home";
 import { Portfolio } from "../pages/portfolio";
 import { ContactUs } from "../pages/contact";
-import { LoginSignUp } from "../pages/login-signup";
+import { Login } from "../pages/login";
 import { About } from "../pages/about";
 import { Socialicons } from "../components/socialicons";
 import { CSSTransition, TransitionGroup } from "react-transition-group";
@@ -23,7 +23,7 @@ const AnimatedRoutes = withRouter(({ location }) => (
       <Routes location={location}>
         <Route exact path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
-        <Route path="/login-signup" element={<LoginSignUp />} />
+        <Route path="/login-signup" element={<Login />} />
         <Route path="/portfolio" element={<Portfolio />} />
         <Route path="/contact" element={<ContactUs />} />
         <Route path="*" element={<Home />} />
