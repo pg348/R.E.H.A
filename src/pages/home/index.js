6876e28cfd4c8c2561bd7ec103094efa83c258cb
akshatshashi <akshatshashi@gmail.com>
2024-02-03 23:10:22 +0530

    Updated the home page with the 3D model layout.

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -4,6 +4,7 @@ import { Helmet, HelmetProvider } from "react-helmet-async";
 import Typewriter from "typewriter-effect";
 import { introdata, meta } from "../../content_option";
 import { Link } from "react-router-dom";
+import Spline from '@splinetool/react-spline'; 
 
 export const Home = () => {
   return (
@@ -15,12 +16,12 @@ export const Home = () => {
           <meta name="description" content={meta.description} />
         </Helmet>
         <div className="intro_sec d-block d-lg-flex align-items-center ">
-          <div
-            className="h_bg-image order-1 order-lg-2 h-100 "
-            style={{ backgroundImage: `url(${introdata.your_img_url})` }}
-          ></div>
+          <div className="h_bg-image order-1 order-lg-2 h-100">
+            {/* Replace the image with Spline component */}
+            <Spline scene="https://prod.spline.design/uFkJjzsj5h8iW49i/scene.splinecode" />
+          </div>
           <div className="text order-2 order-lg-1 h-100 d-lg-flex justify-content-center">
-            <div className="align-self-center ">
+            <div className="align-self-center">
               <div className="intro mx-auto">
                 <h2 className="mb-1x">{introdata.title}</h2>
                 <h1 className="fluidz-48 mb-1x">
@@ -40,7 +41,7 @@ export const Home = () => {
                 <p className="mb-1x">{introdata.description}</p>
                 <div className="intro_btn-action pb-5">
                   <Link to="/portfolio" className="text_2">
-                    <div id="button_p" className="ac_btn btn ">
+                    <div id="button_p" className="ac_btn btn">
                       Tutorial
                       <div className="ring one"></div>
                       <div className="ring two"></div>
@@ -49,7 +50,7 @@ export const Home = () => {
                   </Link>
                   <Link to="/contact">
                     <div id="button_h" className="ac_btn btn">
-                      Contact 
+                      Contact
                       <div className="ring one"></div>
                       <div className="ring two"></div>
                       <div className="ring three"></div>
@@ -64,3 +65,84 @@ export const Home = () => {
     </HelmetProvider>
   );
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import React from "react";
+// import "./style.css";
+// import { Helmet, HelmetProvider } from "react-helmet-async";
+// import Typewriter from "typewriter-effect";
+// import { introdata, meta } from "../../content_option";
+// import { Link } from "react-router-dom";
+
+// export const Home = () => {
+//   return (
+//     <HelmetProvider>
+//       <section id="home" className="home">
+//         <Helmet>
+//           <meta charSet="utf-8" />
+//           <title> {meta.title}</title>
+//           <meta name="description" content={meta.description} />
+//         </Helmet>
+//         <div className="intro_sec d-block d-lg-flex align-items-center ">
+//           <div
+//             className="h_bg-image order-1 order-lg-2 h-100 "
+//             style={{ backgroundImage: `url(${introdata.your_img_url})` }}
+//           ></div>
+//           <div className="text order-2 order-lg-1 h-100 d-lg-flex justify-content-center">
+//             <div className="align-self-center ">
+//               <div className="intro mx-auto">
+//                 <h2 className="mb-1x">{introdata.title}</h2>
+//                 <h1 className="fluidz-48 mb-1x">
+//                   <Typewriter
+//                     options={{
+//                       strings: [
+//                         introdata.animated.first,
+//                         introdata.animated.second,
+//                         introdata.animated.third,
+//                       ],
+//                       autoStart: true,
+//                       loop: true,
+//                       deleteSpeed: 10,
+//                     }}
+//                   />
+//                 </h1>
+//                 <p className="mb-1x">{introdata.description}</p>
+//                 <div className="intro_btn-action pb-5">
+//                   <Link to="/portfolio" className="text_2">
+//                     <div id="button_p" className="ac_btn btn ">
+//                       Tutorial
+//                       <div className="ring one"></div>
+//                       <div className="ring two"></div>
+//                       <div className="ring three"></div>
+//                     </div>
+//                   </Link>
+//                   <Link to="/contact">
+//                     <div id="button_h" className="ac_btn btn">
+//                       Contact 
+//                       <div className="ring one"></div>
+//                       <div className="ring two"></div>
+//                       <div className="ring three"></div>
+//                     </div>
+//                   </Link>
+//                 </div>
+//               </div>
+//             </div>
+//           </div>
+//         </div>
+//       </section>
+//     </HelmetProvider>
+//   );
+// };
